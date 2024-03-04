6d8ef5afc9dfede1c8993d1a62adabba928c89c1
Raj Yadav <rajyadav7047@gmail.com>
2024-03-04 21:13:59 +0530

    add 14 Project

---
+++ 14_Projects/shoppingredux/src/redux/slices/CounterSlice.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: 0,
+};
+
+// step 1 : create createSlice() and write three thing
+// 1. name
+// 2. initialState
+// 3. reducers isme functionality & defination set hoti hai and then export
+
+export const CounterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
+
+// createSlice 2 thing provide krta hn 1. reducer 2. action creator
+
+// Action creators are generated for each case reducer function
+// action creator me humre function ka behaviour store ho jata hn
+export const { increment, decrement } = CounterSlice.actions;
+
+export default CounterSlice.reducer;
